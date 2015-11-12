c833006a340cc4646b7c4c7bd3c6b0d6ae52fc48
xiefei001 <xiefei001@hotmail.com>
2015-11-12 16:41:03 +0100

    update

--- src/main/java/de/swm/springsec/demo/DemoApplication.java
+++ src/main/java/de/swm/springsec/demo/DemoApplication.java
@@ -2,11 +2,30 @@ package de.swm.springsec.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	@RequestMapping("/resources")
+	public Map<String, String> home() {
+		Map<String, String> model = new HashMap<String, String>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 }
--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -2,6 +2,10 @@
  * Created by xie on 11.11.2015.
  */
 angular.module('hello', [])
-    .controller('Home', function () {
-        this.greeting = {id: 'abc', content: 'Hello World!'}
-    });
+    .controller('Home', ['$http', function ($http) {
+        var that = this;
+        $http.get('/resources/').then(function (response) {
+            that.greeting = response.data;
+        });
+        //this.greeting = {id: 'abc', content: 'Hello World!'}
+    }]);
